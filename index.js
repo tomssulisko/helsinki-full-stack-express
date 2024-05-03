c5ef236aceb3ea3ec1baa45d8223c150783b66b5
TWODAY-TG\Tomi <tomi.groning@twoday.com>
2024-05-03 15:23:27 +0300

    exercise 3.1

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: "1"
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: "2"
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: "3"
+  },
+  {
+    name: "Merja Pallas",
+    number: "060-453573436",
+    id: "4"
+  }
+]
+
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+  
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number( request.params.id)
+    const person = persons.find(person => {
+      console.log(person.id, typeof person.id, id, typeof id, person.id === id)
+      return person.id === id
+    })
+    if (person) {
+      console.log(person)
+    response.json(person)  
+    } else {
+        response.status(404).end()
+    } 
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const person = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
